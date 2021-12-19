75046481c1c82dd9b5faac37b3bc15a9daea126c
baici1 <249337001@qq.com>
2021-12-19 15:35:04 +0800

    更新readme.md

--- app/utils/gin_release/gin_release.go
+++ app/utils/gin_release/gin_release.go
@@ -31,6 +31,7 @@ func ReleaseRouter() *gin.Engine {
 // CustomRecovery 自定义错误(panic等)拦截中间件、对可能发生的错误进行拦截、统一记录
 func CustomRecovery() gin.HandlerFunc {
 	DefaultErrorWriter := &PanicExceptionRecord{}
+	//对发生的错误进行恢复，并记录到日志中
 	return gin.RecoveryWithWriter(DefaultErrorWriter, func(c *gin.Context, err interface{}) {
 		// 这里针对发生的panic等异常进行统一响应即可
 		// 这里的 err 数据类型为 ：runtime.boundsError  ，需要转为普通数据类型才可以输出
